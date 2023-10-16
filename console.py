20333b6433ee81e9c32aa521afabebd866d801df
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-16 00:34:12 +0000

    add update method to console

--- console.py
+++ console.py
@@ -91,11 +91,47 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-    def do_update(self, args):
+    def do_update(self, arg):
         """Updates an instance based on the class name and id
         by adding or updating attribute (save the change into the JSON file)
         Ex: $ update BaseModel 1234-1234-1234 email 'aibnb@mail.com'
         """
+        if arg:
+            try:
+                args = arg.split()
+                cls_name = args[0]
+                cls = storage.get_class(cls_name)
+                if len(args) < 2:
+                    if not cls:
+                        print("** class doesn't exist **")
+                    else:
+                        print('** instance id missing **')
+                elif len(args) < 3:
+                    obj_id = args[1]
+                    objs = storage.all()
+                    key = f"{cls_name}.{obj_id}"
+                    if key in objs:
+                        print('** attribute name missing **')
+                    else:
+                        print('** no instance found **')
+                else:
+                    obj_id = args[1]
+                    obj_attr_name = args[2]
+                    objs = storage.all()
+                    # check class name is valid
+                    key = f"{cls_name}.{obj_id}"
+                    if key in objs:
+                        if obj_attr_name in objs[key].__dict__:
+                            objs[key].__dict__[obj_attr_name] = args[3]
+                            storage.save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print('** no instance found **')
+            except NameError:
+                print("** class doesn't exist **")
+        else:
+            print('** class name missing **')
 
     def emptyline(self):
         """Called when an empty line is entered"""
