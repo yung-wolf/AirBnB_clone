68fca1adb5ab35d06762f51dd369943ffd0c5527
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-10-16 00:18:22 +0000

    create test_file_storage.py in test_engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+module: test_file_storage
+
+unittest of FileStorage class
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test file_storage"""
+
+    def setUp(self):
+        """set up test class"""
+        self.fs = FileStorage()
+        self.objs_dict = self.fs.all()
+
+    def test_all(self):
+        """Test all method"""
+        self.assertIsNotNone(self.objs_dict)
+        self.assertEqual(type(self.objs_dict), dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
